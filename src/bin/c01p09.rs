a8137a2aa34cbe0acdd021538425417103fac4ef
Brenden Matthews <brenden@diddyinc.com>
2019-02-16 17:03:50 -0500

    Add chapter 1 problem 9.

---
+++ src/bin/c01p09.rs
@@ -0,0 +1,21 @@
+#[cfg(test)]
+
+fn is_rotated_string(s1: &str, s2: &str) -> bool {
+    let triplet = format!("{}{}{}", s1, s1, s1);
+
+    triplet.contains(s2)
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_string_rotation() {
+        assert_eq!(is_rotated_string("waterbottle", "erbottlewat"), true);
+        assert_eq!(is_rotated_string("herpderp", "lolbob"), false);
+    }
+}
+
+fn main() {
+
+}
