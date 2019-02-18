424924863c9c4240f7b30f2490f7eb321bec147f
Brenden Matthews <brenden@diddyinc.com>
2019-02-18 18:04:52 -0500

    _Actually_ add chapter 2 problem 2.

---
+++ src/bin/c02p02.rs
@@ -0,0 +1,146 @@
+use std::cell::RefCell;
+use std::fmt::Display;
+use std::rc::Rc;
+
+type NodeRef<T> = Rc<RefCell<Node<T>>>;
+
+struct LinkedList<T> {
+    head: Option<NodeRef<T>>,
+}
+
+struct Node<T> {
+    data: T,
+    next: Option<NodeRef<T>>,
+}
+
+struct Iter<T> {
+    next: Option<NodeRef<T>>,
+}
+
+impl<T> Node<T> {
+    fn tail(node: &NodeRef<T>) -> Option<NodeRef<T>> {
+        if let Some(cur) = node.borrow().next.as_ref().cloned() {
+            return Node::tail(&cur.clone());
+        }
+        Some(node.clone())
+    }
+}
+
+impl<T> LinkedList<T>
+where
+    T: std::cmp::Eq,
+    T: std::hash::Hash,
+    T: std::clone::Clone,
+{
+    fn new() -> Self {
+        Self { head: None }
+    }
+
+    fn append(&mut self, new_value: T) {
+        if let Some(tail) = self.tail() {
+            tail.borrow_mut().next = Some(Rc::new(RefCell::new(Node {
+                data: new_value,
+                next: None,
+            })));
+        } else {
+            self.head = Some(Rc::new(RefCell::new(Node {
+                data: new_value,
+                next: None,
+            })));
+        }
+    }
+
+    fn tail(&self) -> Option<NodeRef<T>> {
+        if let Some(cur) = self.head.as_ref().cloned() {
+            if cur.borrow().next.is_none() {
+                return Some(cur.clone());
+            } else {
+                return Node::tail(&cur.clone());
+            }
+        }
+        None
+    }
+
+    fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_ref().cloned(),
+        }
+    }
+
+    fn kth_to_last(&self, k: i32) -> Option<NodeRef<T>> {
+        let mut c = 0;
+        for _ in self.iter() {
+            c += 1;
+        }
+        let stop_at: i32 = c - k;
+        if stop_at < 0 {
+            return None;
+        }
+        c = 0;
+        for node in self.iter() {
+            c += 1;
+            if c >= stop_at {
+                return Some(node.clone());
+            }
+        }
+        None
+    }
+}
+
+impl<'a, T> Iterator for Iter<T> {
+    type Item = NodeRef<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(cur) = self.next.as_ref().cloned() {
+            self.next = cur.borrow().next.clone();
+            return Some(cur.clone());
+        }
+        None
+    }
+}
+
+impl<T: Display> Display for LinkedList<T> {
+    fn fmt(&self, w: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+        write!(w, "[")?;
+        let mut node = self.head.clone();
+        while let Some(n) = node {
+            write!(w, "{}", n.borrow().data)?;
+            node = n.borrow().next.clone();
+            if node.is_some() {
+                write!(w, ", ")?;
+            }
+        }
+        write!(w, "]")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_kth_to_last_element() {
+        let mut list1 = LinkedList::<String>::new();
+        list1.append(String::from("item1"));
+        list1.append(String::from("item2"));
+        list1.append(String::from("item3"));
+        list1.append(String::from("item4"));
+        list1.append(String::from("item5"));
+
+        assert_eq!(
+            list1.kth_to_last(1).unwrap().borrow().data,
+            String::from("item4")
+        );
+        assert_eq!(
+            list1.kth_to_last(2).unwrap().borrow().data,
+            String::from("item3")
+        );
+    }
+}
+
+fn main() {
+    let mut list = LinkedList::<String>::new();
+    list.append(String::from("item1"));
+    list.append(String::from("item2"));
+    list.kth_to_last(1);
+}
