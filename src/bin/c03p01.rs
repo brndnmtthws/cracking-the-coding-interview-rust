c11fa04adf6d91719e60de4553b6bbb4ee38c2af
Brenden Matthews <brenden@diddyinc.com>
2019-03-03 17:32:34 -0500

    Add chapter 3 problem 2.

--- src/bin/c03p01.rs
+++ src/bin/c03p01.rs
@@ -1,86 +1,87 @@
 #[derive(Debug)]
 struct Pointer {
-  start: usize,
-  end: usize,
+    start: usize,
+    end: usize,
 }
 
 #[derive(Debug)]
 struct ThreeInOne<T> {
-  arr: Vec<T>,
-  pointers: Vec<Pointer>,
+    arr: Vec<T>,
+    pointers: Vec<Pointer>,
 }
 
 impl<T> ThreeInOne<T> {
-  fn new() -> Self {
-    ThreeInOne {
-      arr: Vec::<T>::new(),
-      pointers: vec![],
-    }
-  }
-
-  fn push(&mut self, stack: usize, value: T) {
-    let last_end = if self.pointers.is_empty() {
-      0
-    } else {
-      self.pointers.last().unwrap().end
-    };
-    if self.pointers.len() < stack + 1 {
-      self.pointers.resize_with(stack + 1, || Pointer {
-        start: last_end,
-        end: last_end,
-      });
+    fn new() -> Self {
+        ThreeInOne {
+            arr: Vec::<T>::new(),
+            pointers: vec![],
+        }
     }
 
-    self.arr.insert(self.pointers[stack].start, value);
-    self.pointers[stack].end += 1;
-    for pointer in self.pointers.iter_mut().skip(stack + 1) {
-      pointer.start += 1;
-      pointer.end += 1;
-    }
-  }
+    fn push(&mut self, stack: usize, value: T) {
+        let last_end = if self.pointers.is_empty() {
+            0
+        } else {
+            self.pointers.last().unwrap().end
+        };
+        if self.pointers.len() < stack + 1 {
+            self.pointers.resize_with(stack + 1, || Pointer {
+                start: last_end,
+                end: last_end,
+            });
+        }
 
-  fn pop(&mut self, stack: usize) -> Option<T> {
-    if self.pointers.len() < stack + 1 || self.pointers[stack].start == self.pointers[stack].end {
-      return None;
+        self.arr.insert(self.pointers[stack].start, value);
+        self.pointers[stack].end += 1;
+        for pointer in self.pointers.iter_mut().skip(stack + 1) {
+            pointer.start += 1;
+            pointer.end += 1;
+        }
     }
-    let result = self.arr.remove(self.pointers[stack].start);
-    self.pointers[stack].end -= 1;
-    for pointer in self.pointers.iter_mut().skip(stack + 1) {
-      pointer.start -= 1;
-      pointer.end -= 1;
+
+    fn pop(&mut self, stack: usize) -> Option<T> {
+        if self.pointers.len() < stack + 1 || self.pointers[stack].start == self.pointers[stack].end
+        {
+            return None;
+        }
+        let result = self.arr.remove(self.pointers[stack].start);
+        self.pointers[stack].end -= 1;
+        for pointer in self.pointers.iter_mut().skip(stack + 1) {
+            pointer.start -= 1;
+            pointer.end -= 1;
+        }
+        Some(result)
     }
-    Some(result)
-  }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_threeinone() {
-    let mut three_in_one: ThreeInOne<i32> = ThreeInOne::new();
-    three_in_one.push(0, 1);
-    three_in_one.push(1, 2);
-    three_in_one.push(2, 3);
-    assert_eq!(three_in_one.pop(2).unwrap(), 3);
-    assert_eq!(three_in_one.pop(1).unwrap(), 2);
-    assert_eq!(three_in_one.pop(0).unwrap(), 1);
+    #[test]
+    fn test_threeinone() {
+        let mut three_in_one: ThreeInOne<i32> = ThreeInOne::new();
+        three_in_one.push(0, 1);
+        three_in_one.push(1, 2);
+        three_in_one.push(2, 3);
+        assert_eq!(three_in_one.pop(2).unwrap(), 3);
+        assert_eq!(three_in_one.pop(1).unwrap(), 2);
+        assert_eq!(three_in_one.pop(0).unwrap(), 1);
 
-    three_in_one.push(0, 4);
-    three_in_one.push(1, 5);
-    three_in_one.push(2, 6);
-    three_in_one.push(0, 7);
-    three_in_one.push(1, 8);
-    three_in_one.push(2, 9);
-    assert_eq!(three_in_one.pop(2).unwrap(), 9);
-    assert_eq!(three_in_one.pop(1).unwrap(), 8);
-    assert_eq!(three_in_one.pop(0).unwrap(), 7);
-  }
+        three_in_one.push(0, 4);
+        three_in_one.push(1, 5);
+        three_in_one.push(2, 6);
+        three_in_one.push(0, 7);
+        three_in_one.push(1, 8);
+        three_in_one.push(2, 9);
+        assert_eq!(three_in_one.pop(2).unwrap(), 9);
+        assert_eq!(three_in_one.pop(1).unwrap(), 8);
+        assert_eq!(three_in_one.pop(0).unwrap(), 7);
+    }
 }
 
 fn main() {
-  let mut three_in_one: ThreeInOne<i32> = ThreeInOne::new();
-  three_in_one.push(0, 1);
-  three_in_one.pop(0);
+    let mut three_in_one: ThreeInOne<i32> = ThreeInOne::new();
+    three_in_one.push(0, 1);
+    three_in_one.pop(0);
 }
---
+++ src/bin/c03p02.rs
@@ -0,0 +1,74 @@
+#[derive(Debug)]
+struct Stack<T> {
+  arr: Vec<T>,
+  min: Option<T>,
+}
+
+impl<T> Stack<T>
+where
+  T: std::cmp::PartialEq,
+  T: std::cmp::PartialOrd,
+  T: std::clone::Clone,
+{
+  fn new() -> Self {
+    Stack {
+      arr: Vec::<T>::new(),
+      min: None,
+    }
+  }
+
+  fn min(&self) -> Option<&T> {
+    self.min.as_ref()
+  }
+
+  fn push(&mut self, value: T) {
+    if self.min.is_none() || value < *self.min.as_ref().unwrap() {
+      self.min = Some(value.clone());
+    }
+    self.arr.push(value.clone());
+  }
+
+  fn pop(&mut self) -> Option<T> {
+    let result = self.arr.pop();
+    if self.arr.is_empty() {
+      self.min = None
+    } else {
+      self.min = Some(self.arr.first().unwrap().clone());
+      for value in self.arr.iter().skip(1) {
+        if value < self.min.as_ref().unwrap() {
+          self.min = Some(value.clone());
+        }
+      }
+    }
+    result
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_stack_with_min() {
+    let mut stack_with_min: Stack<i32> = Stack::new();
+    stack_with_min.push(3);
+    assert_eq!(*stack_with_min.min().unwrap(), 3);
+    stack_with_min.push(2);
+    assert_eq!(*stack_with_min.min().unwrap(), 2);
+    stack_with_min.push(1);
+    assert_eq!(*stack_with_min.min().unwrap(), 1);
+    assert_eq!(stack_with_min.pop().unwrap(), 1);
+    assert_eq!(*stack_with_min.min().unwrap(), 2);
+    assert_eq!(stack_with_min.pop().unwrap(), 2);
+    assert_eq!(*stack_with_min.min().unwrap(), 3);
+    assert_eq!(stack_with_min.pop().unwrap(), 3);
+    assert_eq!(stack_with_min.min().is_none(), true);
+  }
+}
+
+fn main() {
+  let mut stack_with_min: Stack<i32> = Stack::new();
+  stack_with_min.push(1);
+  stack_with_min.min();
+  stack_with_min.pop();
+}
