45ffa3e91c8544e70640b760e67612738db2b7a3
Brenden Matthews <brenden@diddyinc.com>
2019-03-17 16:23:38 -0400

    Add chapter 5 problem 7.

---
+++ src/bin/c05p07.rs
@@ -0,0 +1,25 @@
+fn bitwise_swap(a: &mut i32, b: &mut i32) {
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bitwise_swap() {
+        let mut a = 0b110;
+        let mut b = 0b001;
+        bitwise_swap(&mut a, &mut b);
+        assert_eq!(a, 0b001);
+        assert_eq!(b, 0b110);
+    }
+}
+
+fn main() {
+    let mut a = 0b110;
+        let mut b = 0b001;
+        bitwise_swap(&mut a, &mut b);
+}
