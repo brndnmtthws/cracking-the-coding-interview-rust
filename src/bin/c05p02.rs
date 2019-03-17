69adb05aca293d779cede5c6491ffabe332841c8
Brenden Matthews <brenden@diddyinc.com>
2019-03-17 15:24:26 -0400

    Add chapter 5 problem 2.

---
+++ src/bin/c05p02.rs
@@ -0,0 +1,25 @@
+fn float_bits_to_str(n: f64) -> String {
+    let bits = n.to_bits();
+    let mut ret = String::new();
+    for i in 0..64 {
+        if (1 << (63 - i)) & bits != 0 {
+        ret.push('1');
+        } else {
+        ret.push('0');
+        }
+    }
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_float_to_string() {
+        assert_eq!(float_bits_to_str(0.72), "0011111111100111000010100011110101110000101000111101011100001010");
+    }
+}
+
+fn main() {
+}
