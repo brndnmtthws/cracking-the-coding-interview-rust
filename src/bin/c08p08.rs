00cbe6ccb13094555f1df34bf883ae94852932f5
Brenden Matthews <brenden@diddyinc.com>
2019-04-06 13:54:03 -0400

    Fix C08P08 name.

--- src/bin/c08p08.rs
+++ src/bin/c08p08.rs
@@ -1,4 +1,4 @@
-fn permutations_without_dups(s: &str) -> Vec<String> {
+fn permutations_with_dups(s: &str) -> Vec<String> {
     if s.is_empty() {
         vec![]
     } else if s.len() == 1 {
@@ -6,7 +6,7 @@ fn permutations_without_dups(s: &str) -> Vec<String> {
     } else if s.len() == 2 {
         vec![s.to_string(), s.chars().rev().collect()]
     } else {
-        let subperms = permutations_without_dups(&s[1..]);
+        let subperms = permutations_with_dups(&s[1..]);
         let item = s.chars().next().unwrap();
         subperms
             .iter()
@@ -31,14 +31,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_permutations_without_dups() {
+    fn test_permutations_with_dups() {
         let s1 = "abb";
         let permutations = vec!["abb", "bab", "bba", "abb", "bab", "bba"];
-        assert_eq!(permutations_without_dups(&s1), permutations);
+        assert_eq!(permutations_with_dups(&s1), permutations);
     }
 }
 
 fn main() {
     let s1 = "abc";
-    permutations_without_dups(&s1);
+    permutations_with_dups(&s1);
 }
