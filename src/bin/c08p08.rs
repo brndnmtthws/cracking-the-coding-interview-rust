45b5e0470a86225beb3616d57baeea9609357353
Brenden Matthews <brenden@diddyinc.com>
2019-04-06 13:50:16 -0400

    Add chapter 8 problem 8.

---
+++ src/bin/c08p08.rs
@@ -0,0 +1,44 @@
+fn permutations_without_dups(s: &str) -> Vec<String> {
+    if s.is_empty() {
+        vec![]
+    } else if s.len() == 1 {
+        vec![s.to_string()]
+    } else if s.len() == 2 {
+        vec![s.to_string(), s.chars().rev().collect()]
+    } else {
+        let subperms = permutations_without_dups(&s[1..]);
+        let item = s.chars().next().unwrap();
+        subperms
+            .iter()
+            .flat_map(|substr| {
+                let mut new_subperms: Vec<String> = vec![];
+                for (idx, _) in substr.chars().enumerate() {
+                    let mut s = substr.clone();
+                    s.insert(idx, item);
+                    new_subperms.push(s);
+                }
+                let mut s = substr.clone();
+                s.push(item);
+                new_subperms.push(s);
+                new_subperms
+            })
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_permutations_without_dups() {
+        let s1 = "abb";
+        let permutations = vec!["abb", "bab", "bba", "abb", "bab", "bba"];
+        assert_eq!(permutations_without_dups(&s1), permutations);
+    }
+}
+
+fn main() {
+    let s1 = "abc";
+    permutations_without_dups(&s1);
+}
